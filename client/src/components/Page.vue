b46be2d93ca09d70370a2df08828c49b7d3bcb05
Ekaterina <pirit544@gmail.com>
2020-11-15 23:10:14 +0500

    connect flask with vue

---
+++ client/src/components/Page.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <p>{{ msg }}</p>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'Page',
+  data () {
+    return {
+      msg: ''
+    }
+  },
+  methods: {
+    getMessage () {
+      const path = 'http://localhost:5000/'
+      axios.get(path)
+        .then((res) => {
+          this.msg = res.data
+        })
+        .catch((error) => {
+          // eslint-выключение следующей строки
+          console.error(error)
+        })
+    }
+  },
+  created () {
+    this.getMessage()
+  }
+}
+</script>
--- server/app.py
+++ server/app.py
@@ -12,9 +12,9 @@ app.config.from_object(__name__)
 CORS(app)
 
 # sanity check route
-@app.route('/ping', methods=['GET'])
-def ping_pong():
-    return jsonify('pong!')
+@app.route('', methods=['GET'])
+def landing():
+    return jsonify('')
 
 if __name__ == '__main__':
     app.run()
